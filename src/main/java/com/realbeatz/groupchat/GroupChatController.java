9adb44bd400f07b3421c17840b75a059b4ce0529
Sun Kit Tsui <sunkittsui@gmail.com>
2022-11-30 12:31:54 -0600

    Limit GroupChatController access to Super Admin

--- src/main/java/com/realbeatz/groupchat/GroupChatController.java
+++ src/main/java/com/realbeatz/groupchat/GroupChatController.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,6 +15,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @Slf4j
+@PreAuthorize("hasRole('ROLE_SUPER_ADMIN')")
 @RequestMapping("/api/group-chat")
 public class GroupChatController {
 
